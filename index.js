4bd7cbb50fca2f73bbe0cef4b1f042917423b61a
Leguminous <murayaleo911@gmail.com>
2024-10-16 17:08:18 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+};
+
+const result = returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']);
+console.log(result); 
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+};
+
+const last = returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']);
+console.log(last); 
+
+const selectingDrivers= [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+];
+
+const createFareMultiplier = function(multiplier) {
+    return function(fare) {
+        return fare * multiplier;
+    };
+};
+
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(drivers, callback) {
+    return callback(drivers);
+  };
+
+  console.log(selectDifferentDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'], returnFirstTwoDrivers)); 
+  console.log(selectDifferentDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'], returnLastTwoDrivers)); 
